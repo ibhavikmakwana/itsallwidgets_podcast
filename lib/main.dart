19a9ebaafc2432ef5388aa98966485aa15b72f9b
bhavik <bhavikmakwana43@gmail.com>
2019-08-11 21:43:29 +0530

    added new previews and graphic.

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: appName,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'MajorMonoDisplay',
       ),
